8ce032d8868608917ba395415255db396ab372de
Lysann Schlegel <lysann.schlegel@outlook.com>
2015-04-15 22:52:37 +0200

    Add bridging header in order to access Objetive-C dependencies

---
+++ Examples/Swift/Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  Bridging-Header.h
+//  SwiftExample
+//
+//  Created by Lysann Schlegel on 4/15/15.
+//  Copyright (c) 2015 Lysann Schlegel. All rights reserved.
+//
+
+#import <GooglePlus/GooglePlus.h>
+#import <GooglePlusShareActivity/GPPShareActivity.h>
