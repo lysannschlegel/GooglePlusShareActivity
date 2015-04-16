6b0a7a03de0a1f637818f180655cd15556e82218
Lysann Schlegel <lysann.schlegel@outlook.com>
2015-04-17 00:13:37 +0200

    Make documentation get picket up by appledoc

--- GooglePlusShareActivity/GPPShareActivity.h
+++ GooglePlusShareActivity/GPPShareActivity.h
@@ -1,6 +1,6 @@
 // GPPShareActivity.h
 //
-// Copyright (c) 2013-2014 Lysann Schlegel (https://github.com/lysannschlegel)
+// Copyright (c) 2013-2015 Lysann Schlegel (https://github.com/lysannschlegel)
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -27,25 +27,45 @@
 extern NSString *const GPPShareActivityType;
 
 
-// An UIActivity that opens a share dialog for Google+.
-// Item types accepted:
-//   - NSString instances set the prefilled text of the share dialog. Any instance overrides
-//     the previous one.
-//   - UIImage instances are attached to the share dialog. Multiple instances can be attached.
-//   - NSURL instances set the URL to be shared. Any instance overrides the previous one.
-//   - Objects that conform to the GPPShareBuilder protocol override any other items, and
-//     instead use only the item to create the share dialog. Any instance overrides the previous
-//     one.
+/**
+ A `UIActivity` that opens a share dialog for Google+.
+
+ Please see the `README.md` file for detailed instructions on using this class.
+
+ Accepted activity item types:
+
+ - `NSString` instances set the prefilled text of the share dialog. Any instance overrides
+   the previous one.
+ - `UIImage` instances are attached to the share dialog. Multiple instances can be attached.
+ - `NSURL` instances set the URL to be shared. Any instance overrides the previous one.
+ - Objects that conform to the `GPPShareBuilder` protocol override any other items, and
+   instead use only the item to create the share dialog. Any instance overrides the previous
+   one.
+ */
 @interface GPPShareActivity : UIActivity
 
-// Set one of these to dismiss the popover view controller (iPad) or presented view
-// controller (iPhone) before initiating the Google+ sharing
+/** @name Auto-dismissing the UIActivityViewController */
+
+/**
+ If set, this controller will be dismissed before starting the Google+ sharing sequence.
+ Use it to automatically dismiss the popover controller that presents the
+ `UIActivityViewController` (on iPad).
+ */
 @property (weak, nonatomic) UIPopoverController* activityPopoverViewController;
+/**
+ If set, the `presentedViewController` of this property will be dismissed before starting
+ the Google+ sharing sequence. Use it to automatically dismiss the `UIActivityViewController`
+ (on iPhone).
+ */
 @property (weak, nonatomic) UIViewController* activitySuperViewController;
 
-// Set to YES if the activity should be enabled even when there are no items it can share.
-// The Google+ sharing form will then be empty, but can be filled by the user.
-// Default is NO.
+/** @name Enable activity even if no items match */
+
+/**
+ Set to `YES` if the activity should be enabled even when there are no items it can share.
+ The Google+ sharing form will then be empty, but can be filled by the user.
+ Default is `NO`.
+ */
 @property (assign, nonatomic) BOOL canShowEmptyForm;
 
 @end
